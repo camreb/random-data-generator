b41e98ab03713bf131c36ff2380cd65dbf17c68f
Kamil <kamreb4@gmail.com>
2024-05-13 07:56:26 +0200

    Updade db_manage_commands.py

--- data_generator/db_manage_commands.py
+++ data_generator/db_manage_commands.py
@@ -6,7 +6,7 @@ from data_generator import app, db
 
 
 def load_csv_data(file_name: str) -> pd.DataFrame:
-    csv_path = Path(__file__).resolve().parent.parent / 'data' / file_name
+    csv_path = Path(__file__).resolve().parent.parent / 'sample_data' / file_name
     df = pd.read_csv(csv_path, sep=';', index_col=False)
     # df.reset_index(drop=True, inplace=True)
     return df
@@ -20,11 +20,12 @@ def db_manage():
 
 @db_manage.command()
 def add_data():
-    """Add data to database"""
+    """Add sample_data to database"""
 
     sql_engine = db.engine
     try:
         data_csv = load_csv_data('first_name.csv')
+        data_csv['gender'] = data_csv['gender'].astype('category')
         data_csv.to_sql('first_names', sql_engine, if_exists='append', index=False)
 
         data_csv = load_csv_data('last_name.csv')
@@ -45,16 +46,10 @@ def add_data():
 
 @db_manage.command()
 def remove_data():
-    """Remove all data from the database"""
+    """Remove all sample_data from the database"""
     try:
         db.session.execute(text('TRUNCATE TABLE first_names;'))
         db.session.execute(text('TRUNCATE TABLE last_names;'))
-        # TRUNCATE TABLE - komenda nie zostanie poprawnie wykonana na tabelach, w których jest klucz obcy
-
-        # db.session.execute(text('DELETE FROM books;'))
-        # db.session.execute(text('ALTER TABLE books AUTO_INCREMENT = 1;'))
-        # db.session.execute(text('DELETE FROM authors;'))
-        # db.session.execute(text('ALTER TABLE authors AUTO_INCREMENT = 1;'))
 
         db.session.commit()
         print('Data has been successfully removed from database')
