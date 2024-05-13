0d62ec995068a6ae4674628f233c292e64f92a18
Kamil <kamreb4@gmail.com>
2024-05-13 08:16:27 +0200

    Update location.py

--- data_generator/fields/location.py
+++ data_generator/fields/location.py
@@ -1,55 +1,26 @@
 import numpy as np
-import pandas as pd
 
-from data_generator.utils import read_sql, measure_func_time, generate_email_list
-
-
-@measure_func_time
-def location_fields(set_field: set, size: int) -> pd.DataFrame:
-    if set_field:
-        set_field = list(set_field)
-        df_main = pd.DataFrame(columns=set_field)
-        df = read_sql('location')
-        if len(set_field) == 1:
-            column = set_field[0]
-            df_main[column] = np.random.choice(df[column], size)
-        else:
-            index_list = np.random.choice(list(range(len(df.index))), size=size, replace=True)
-            for column in set_field:
-                column_values = [df.loc[x, column] for x in index_list]
-                df_main[column] = column_values
-        return df_main
-
+from data_generator.utils import measure_func_time
+from sqlalchemy.sql import text
+from data_generator import db
 
 
 @measure_func_time
-def personal_fields(set_field: set, size: int) -> pd.DataFrame:
-    df_main = pd.DataFrame(columns=set_field)
+def location_fields(set_field: list, size: int):
 
-    if set_field:
-        set_field = list(set_field)
-        df_main = pd.DataFrame(columns=set_field)
-        gender_list = np.random.choice(np.array(['M', 'K']), size, True)
-        df_main['gender'] = gender_list
+    response = db.session.execute(text('SELECT city, voivodeship from locations;'))
+    cities = []
+    voivodeships = []
+    for elem in response.all():
+        cities.append(elem[0])
+        voivodeships.append(elem[1])
 
-    for column in set_field:
-        if column in ['gender', 'email']:
-            continue
-        df = read_sql(column)
-        gender_list = list(gender_list)
-        men_column = df.query('gender == "M"')[column].tolist()
-        women_column = df.query('gender == "K"')[column].tolist()
+    index_list = np.random.choice(list(range(len(cities))), size=size, replace=True)
 
-        column_list = [np.random.choice(men_column) if gender_list[i] == 'M'
-                       else np.random.choice(women_column) for i in range(size)]
-        df_main[column] = column_list
-    if 'gender' not in set_field:
-        df_main.drop(['gender'], axis=1)
-    if 'email' in set_field:
-        first_n = df_main['first_name']
-        last_n = df_main['last_name']
-        email_list = generate_email_list(first_n, last_n)
-        df_main['email'] = email_list
-
-    return df_main
+    data = {}
+    if 'city' in set_field:
+        data['city'] = [cities[i] for i in index_list]
+    if 'voivodeship' in set_field:
+        data['voivodeship'] = [voivodeships[i] for i in index_list]
+    return data
 
